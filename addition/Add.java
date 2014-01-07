34ce2e1f8ffe94dedefc198aad5174b3dcc72b8f
ryanhburbank <ryanhburbank@gmail.com>
2014-01-06 17:38:10 -0800

    Wrote Add class

---
+++ addition/Add.java
@@ -0,0 +1,13 @@
+public class Add {
+  public static void main(String args[]) {
+    int num; // a variable called num
+    //in java variables must be declared before use
+    num = 100;
+
+    System.out.println("This is num: " + num);
+    num = num * 2;
+
+    System.out.print("The value of num * 2 is: ");
+    System.out.println(num);
+  }
+}
