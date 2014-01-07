6e5d4eedebe38b43133403d56d94919fae265a70
ryanhburbank <ryanhburbank@gmail.com>
2014-01-06 22:40:53 -0800

    touch data type files, write example for boolean in java

---
+++ 5_primitive_data_types/Overview.java
@@ -0,0 +1,46 @@
+/* The 8 Primitive Data types in Java
+Java has 8 primitive types of data:
+1: Boolean(boolean) Size: N/A, Min Val: N/A, Max Value: N/A
+2: Character(char) Size: 16-bit
+3: Byte Integer(byte) Size: 8-bit
+4: Short Integer(short) Size: 16-bit
+5: Integer(int) Size: 32-bit
+6: Long Integer(long) Size: 64-bit
+7: Floating-point(float) Size: 32-bit
+8: Double precision floating-point(double) Size: 64-bit
+
+byte, short, int, and long are whole value signed numbers
+float and double are fractional precision numbers
+char represents symbols in a character set, while
+boolean represents true/false values
+
+  Unsigned numbers can hold a larger positive value, 
+  and no negative value
+
+  Unsigned uses the leading bit, while signed uses the 
+  leftmost bit to identify if a number is + || _
+  signed integer can hold both + && - values
+
+Jave integers(byte, short, int, long) are signed
+and can have positive and negative values
+
+ranges:
+byte: -128 to 127
+short: -32,768 to 32,767
+int: -2,147,483,648 to 2,147,483,647
+long: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+There are two kinds of floating-point types: float & double
+float represents single-precision numbers, while double stores
+double precision numbers
+double 64 bit width
+float 32 bit width
+
+The word double derives from the fact that a 
+double-precision number uses twice as many bits 
+as a regular floating-point number.  For example, 
+if a single-precision number requires 32 bits, 
+its double-precision counterpart will be 64 bits long.
+
+The extra bits increase not only the precision but 
+also the range of magnitudes that can be represented.
---
+++ 5_primitive_data_types/TypeBoolean.java
@@ -0,0 +1,24 @@
+/* Boolean is type returned by all relational
+ operators
+
+ can only have true of false values
+ true and false do not convert into any
+ numerical representation
+
+
+ The Boolean class has the methods for converting
+ a boolean to a String and a String to a boolean
+
+ example program that demonstrates the boolean type
+ */
+
+ public class TypeBoolean {
+  public static void main(String args[]) {
+    //Dynamic initialization
+    boolean x = false;
+
+    System.out.println("b is " + x);
+    x = true;
+    System.out.println("b is " + x);
+  }
+ }
