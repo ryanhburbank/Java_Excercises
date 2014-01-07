a2563f14f2d52b74bf6c6eb7cd36987bc4876191
ryanhburbank <ryanhburbank@gmail.com>
2014-01-07 12:51:38 -0800

    Add notes on float type + code example on floating-point literals

---
+++ 5_primitive_data_types/TypeFloat.java
@@ -0,0 +1,21 @@
+/* 
+Float Type represents single precision numbers.  
+ flot type variables are useful when you need
+ a fractionl component.  Here are some example
+ float variable declarations. 
+ example float declarations: float high, low;
+
+ float is a 32-bit width 
+
+ Literals: Floating point literals default to 
+ doulble precision.  To specify a float literal
+ you must append an F or f to the constant
+
+ example code: declaring a float literal
+*/
+ public class TypeFloat {
+  public static void main(String args[]){
+    float d = 3.14159F; //F ensures single precision
+    System.out.println(d);
+  }
+ }
