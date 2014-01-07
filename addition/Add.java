f0f170d2058b9454e53aa1bd61629d98940d3560
ryanhburbank <ryanhburbank@gmail.com>
2014-01-06 17:43:37 -0800

    Define two variables with comma-seperated list of variable names, change 2nd printout to be sum of both variables

--- addition/Add.java
+++ addition/Add.java
@@ -1,13 +1,13 @@
 public class Add {
   public static void main(String args[]) {
-    int num; // a variable called num
+    int num, num2; // a variable called num
     //in java variables must be declared before use
     num = 100;
+    num2 = 200;
 
     System.out.println("This is num: " + num);
-    num = num * 2;
 
-    System.out.print("The value of num * 2 is: ");
-    System.out.println(num);
+    System.out.print("The sum of " + num + " and " + num2 + " is: ");
+    System.out.println(num + num2);
   }
 }
