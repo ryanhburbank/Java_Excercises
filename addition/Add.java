29f18d1f1e8afb523ebaaecd69abfb1026f1b8a4
ryanhburbank <ryanhburbank@gmail.com>
2014-01-06 17:48:33 -0800

    add if block to main method

--- addition/Add.java
+++ addition/Add.java
@@ -4,6 +4,12 @@ public class Add {
     //in java variables must be declared before use
     num = 100;
     num2 = 200;
+    if (num < num2) { //begining of block
+      num = num2;
+      num2 = 0;
+      System.out.println("num == " + num);
+      System.out.println("num2 == " + num2);
+    } //end of block
 
     System.out.println("This is num: " + num);
 
