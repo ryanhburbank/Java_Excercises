f4fb71bc6d609553ccd09812cc1ea4313b48a14f
ryanhburbank <ryanhburbank@gmail.com>
2014-01-08 12:20:53 -0800

    add notes on object creation through classes in java, creating object instances, and accessing object instance variables, + code examples

--- 9_java_classes/Intro.java
+++ 9_java_classes/Intro.java
@@ -27,3 +27,46 @@ a class called Box that defines three member variables
 class Box {
   int width, height, depth;
 }
+
+When creating a class you are creating a new data type
+you can use this type to declare objects of that type
+
+Syntax: 
+Creating objects of a class is a two step process.
+ Declare a variable of the class type
+ use the new operator to dynamically to dynamically allocate
+ memory for an object. 
+
+ Box mybox = new Box()//declares an object of type box. combines both
+ //steps, can be broken down into two steps as below:
+
+ Box mybox; //declare reference to object of type box.  At this point
+              mybox contains the value null, indiciating it does not yet
+              point to an actualy object, this attempting to use it will result
+              in an error.
+mybox = new Box(); //allocate a Box object and assigns a
+              reference to mybox.  After second line executes
+              mybox can be used as a Box object 
+              mybox holds the memory address of the actual Box object 
+ 
+Example: A class defines a new type of data.  We will create
+a Box object, then create an instance of that object
+*/
+class Box {
+  int width;
+  int height;
+  int depth;
+}
+
+public class Intro {
+  public static void main(String args[]) {
+    Box myBox = new Box();
+    myBox.width = 10;
+
+    System.out.println("myBox.width:" + myBox.width);
+  /* mybox is an instance of Box.  mybox contains its 
+  own copy of each instance variable, width, height, and depth,
+  each originally defined by the class.  To access these variables
+  you the '.' operator
+  }
+}
